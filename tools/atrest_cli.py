3992d8e36f931a6029be5eada22827b114f8ffda
Drew Pihera <drew.pihera@gtri.gatech.edu>
2016-05-18 20:56:57 -0400

    changed to new class name for client, added some untested funtionality.

--- tools/atrest_cli.py
+++ tools/atrest_cli.py
@@ -8,7 +8,10 @@ from collections import OrderedDict
 # the tools directory for now...
 sys.path.append('..')
 
-from AtREST.atrest import APIManager
+from AtREST.atrest import (
+    ConfluenceRESTClient as RESTClient,
+    ClientMode,
+)
 
 # from PythonConfluenceAPI import ConfluenceAPI
 
@@ -25,7 +28,7 @@ class AtRESTCli():
     def __init__(self, *args, **kwargs):
         """
         """
-        self.apiman = APIManager()
+        self._client = None
         self.__build_ops()
 
     def __build_ops(self):
@@ -38,17 +41,25 @@ class AtRESTCli():
         },
         """
         self._OPS = {
+            # TODO: this should just happent at startup. also shouldn't print
+            #       command after it's been done (so shouldn't be a command).
+            #       then the req_api_init stuff becomes unnecessary
             '1': {
-                'label': 'Initialize the Confluence API (must be done first!)',
+                'label': 'Initialize the Confluence API Client (must be done first!)',
                 'op': self.initialize_api,
                 'req_api_init': False,
             },
             '2': {
+                'label': 'Print command explainations/doc',
+                'op': self.command_doc,
+                'req_api_init': False,
+            },
+            '3': {
                 'label': 'List Spaces',
                 'op': self.list_spaces,
                 'req_api_init': True,
             },
-            # '3': ...,
+            # '4': ...,
             '0': {
                 'label': 'Run away screaming (exit)!',
                 'op': self.leave_this_silly_place,
@@ -66,7 +77,7 @@ class AtRESTCli():
         )
 
         for k, v in ordered_opts.items():
-            if v['req_api_init'] == True and self.apiman.status[0] == False:
+            if v['req_api_init'] == True and self._client == None:
                 # don't give access to the things that require an initialized api
                 # until it's been initialized.
                 continue
@@ -82,19 +93,27 @@ class AtRESTCli():
             return
         selop['op']()
 
-    def initialize_api(self):
+    def initialize_client(self):
         """
         """
         uname = input('Enter your username: ')
         url = input('Enter the REST API url base (e.g. https://blah.com/confluence): ')
+        client_mode = input(
+            'Enter run mode (1 for real, anything else for dry): '
+        )
         pw = getpass('Enter password (will not be echoed): ')
 
-        self.apiman.init_api(username=uname, password=pw, url_base=url)
+        m = ClientMode.dry_run
+
+        if client_mode == '1':
+            m = ClientMode.real_run
+
+        self._client = RESTClient(uname, pw, url, mode=m)
 
     def list_spaces(self):
         """
         """
-        space_names = self.apiman.list_space_names()
+        space_names = self._client.list_space_names()
         print('\n\t------------------------------')
         print('\tSpace Names:')
         pprint(space_names)

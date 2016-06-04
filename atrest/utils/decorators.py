6c36a195680f59ae18305e38b4f7bcfa69c42ccc
Drew Pihera <drew.pihera@gtri.gatech.edu>
2016-06-04 18:51:42 -0400

    logging changed to self.log, added decorator for making a kwarg dict for convenience

--- atrest/utils/decorators.py
+++ atrest/utils/decorators.py
@@ -5,7 +5,8 @@ from requests import (
     HTTPError,
 )
 
-
+# TODO: give a way to specify different handling for each type of error. allow
+#       specification of those that should be propagated as well.
 def logs_httperror(wrapped_func):
     """
     Decorator for generic handling of HTTPErrors for methods that can throw
@@ -45,11 +46,44 @@ def debug_log_call(log_args=False, log_kwargs=False):
     def decorator_wrapper(wrapped_func):
         @wraps(wrapped_func)
         def wrapper(self, *args, **kwargs):
-            logging.debug('debug_log_call: Calling %s ', wrapped_func.__name__)
+            self.log.debug('debug_log_call: Calling %s ', wrapped_func.__name__)
             if log_args:
-                logging.debug('\targs: %s ', args)
+                self.log.debug('\targs: %s ', args)
             if log_kwargs:
-                logging.debug('\tkwargs: %s ', kwargs)
+                self.log.debug('\tkwargs: %s ', kwargs)
+            return wrapped_func(self, *args, **kwargs)
+        return wrapper
+    return decorator_wrapper
+
+def makes_kwarg_dict(ignore_keys=None, sub_keys=None):
+    """
+    Decorator to make a dict of explicit keyword arguments of a method or
+    function. This dict wil be available in the method as method.keyword_dict.
+    If ignore_keys list is given, those keys will be omitted from the result.
+    If a sub_keys dict is given, it should map explicit keyword keys to ones
+    that should be subsituted in the resuling dict (the value will remain what
+    was) passed in.
+
+    e.g.:
+
+    @makes_kwarg_dict(ignore_keys=['a'], sub_keys={'c': 'c_sub'})
+    def some_method(self, a, b, c=None, d=None, e=True):
+        # there will be a dict self.some_method.keyword_dict containing
+        # {'sub_c': c, 'd': d}.
+
+    NOTE: This is a convenience for some wrapped api methods, and can lead to
+          hard to maintain/read code. Please don't abuse.
+    """
+    def decorator_wrapper(wrapped_func):
+        @wraps(wrapped_func)
+        def wrapper(self, *args, **kwargs):
+            wrapper.keyword_dict = {}
+            # gets substitute keys: given a key k and dict d, return k if d is
+            # None, d[k] if there, else k.
+            ignore = ignore_keys or []
+            get_key = lambda k, d: d.get(k, k) if d else k
+            matched = [(k, v) for k, v in kwargs.items() if k not in ignore]
+            wrapper.keyword_dict = {get_key(k, sub_keys): v for k, v in matched}
             return wrapped_func(self, *args, **kwargs)
         return wrapper
     return decorator_wrapper

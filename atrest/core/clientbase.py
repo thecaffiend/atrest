f5e97c746901d9d0aab9817505aeb2591ffee3ac
Drew Pihera <drew.pihera@gtri.gatech.edu>
2016-06-04 18:49:48 -0400

    todos, doc, and refactor

--- atrest/core/clientbase.py
+++ atrest/core/clientbase.py
@@ -1,46 +1,59 @@
 import getpass
-import sys
 
-from traitlets.config.configurable import Configurable
 from traitlets import (
-    Enum as TraitletEnum, Unicode, default,
+    Dict, Unicode, default,
 )
 
 from atrest.core.application import (
-    AppRunMode,
     AtRESTCLIApplication,
+    cli_aliases, # TODO: cleaner way to do this, like a base class method...
+    cli_flags, # TODO: cleaner way to do this, like a base class method...
 )
 
-# TODO: extra aliases or flags?
-rc_extra_aliases = {
+from atrest.core.exception import (
+    ResultsException,
+)
+
+client_aliases = {
     'username' : 'AtRESTClientBase.username',
     'apiurlbase' : 'AtRESTClientBase.api_url_base',
 }
+client_aliases.update(cli_aliases)
 
-# rc_extra_flags = dict(
-#     # Format:
-#     #flag = ({'ClassName' : {'trait' : value}},
-#     #    "flag help string"),
-# )
-
+client_flags = {
+}
+client_flags.update(cli_flags)
 
 class AtRESTClientBase(AtRESTCLIApplication):
     """
     Base class for all Atlassian REST API Clients.
     """
-    username = Unicode(help='The username for API auth.').tag(config=True)
+
+    name = 'atrest-clientbase'
+
+    description = """Base class for AtREST REST API Clients that can be run
+    interactively. This should not be used as is, but should be subclassed.
+    """
+
+    examples = """
+    Base class has no examples
+    """
+
+    aliases = Dict(client_aliases)
+    flags = Dict(client_flags)
+
+    # TODO: AtRESTClientOperation also has this username/url need. Figure how
+    #       to define just one place.
+
+    username = Unicode(u'',
+        help='The username for API auth.').tag(config=True)
 
     @default('username')
     def _username_default(self):
         """
+        Called if username is accessed before it's given a value.
         """
         return input('Please enter username: ')
-        # un = ''
-        # if self.interactive:
-        #     un = input('Please enter username: ')
-        # else:
-        #     self.log.error('No username specified for REST Client')
-        # return un
 
     # TODO: for now, the server address is just a unicode string. look into
     #       finding/making traitlets for real server values (urls, ports,
@@ -53,29 +66,19 @@ class AtRESTClientBase(AtRESTCLIApplication):
     @default('api_url_base')
     def _api_url_base_default(self):
         """
+        Called if api_url_base is accessed before it's given a value.
         """
         return input('Please enter the base URL for the API: ')
 
-        # baseurl = ''
-        # if self.interactive:
-        #    baseurl = input('Please enter the base URL for the API: ')
-        # else:
-        #     self.log.error('No API URL specified for REST Client')
-        # return baseurl
-
     # TODO: We don't want it configurable, but being able to specify a trait
-    #       like `__api = Type(SomeAPIBase, help="the api object to use")`
+    #       like `_api = Type(SomeAPIBase, help="the api object to use")`
     #       would be spiffy. there is no base class for Confluence/Jira/Stash
     #       etc apis though.
 
     def __init__(self, *args, **kwargs):
         """
-        Init method for class. Nothing special.
+        Initializer for class. Nothing special.
         """
-        self.update_aliases(rc_extra_aliases)
-
-        super().__init__(*args, **kwargs)
-
         self._api = None
 
     @property
@@ -92,15 +95,15 @@ class AtRESTClientBase(AtRESTCLIApplication):
     def status(self):
         """
         status property getter. return tuple of (bool, msg) where bool is True
-        if the __api member is initialized and False otherwise.
+        if the _api member is initialized and False otherwise.
         """
         return True if self._api else False
 
+# TODO: test if we need this or not
     def initialize(self, *args, **kwargs):
         """
         Initialize the client.
         """
-        # LEFTOFF
         # NOTE: To get subapps and such, parse_command_line must be done
         self.parse_command_line(argv=kwargs.get('argv', None))
 
@@ -108,13 +111,11 @@ class AtRESTClientBase(AtRESTCLIApplication):
         # starting a subapp, no need to init
             return
 
-#        self.initialize_api()
-
     def initialize_api(self, username=None, apiurlbase=None):
         """
         Method to initialize the REST API. All subclasses should implement this
         method, and it is assumed they will all require a username, password,
-        and url_base for the API of interest.
+        and base url for the API of interest.
         """
         # TODO: See if it makes sense to allow params other than
         #       username/password for some APIs. Like a token or something else
@@ -129,8 +130,6 @@ class AtRESTClientBase(AtRESTCLIApplication):
         return getpass.getpass('Please enter password for REST API: ')
 
     def start(self):
-        """
-        """
         """
         Override of Application start method. By default, just checks if we are
         to be run interactively or not and goes from there.
@@ -152,5 +151,4 @@ class AtRESTClientBase(AtRESTCLIApplication):
         if self.interactive:
             self._start_interactive()
         else:
-#            self.initialize_api()
             self._start_normal()
